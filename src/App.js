3352162dbe2c2f194aaecc4149d1cf5a8c6e71a6
Slys <slysdeveloper@gmail.com>
2023-06-09 13:37:51 -0400

    Converted App component --> function

--- src/App.js
+++ src/App.js
@@ -1,48 +1,31 @@
 import "./styles/App.css";
-import { Component } from "react";
+import { Component, useState } from "react";
 import GeneralInformation from "./components/GeneralInformation";
 import EducationalExperience from "./components/EducationalExperience";
 import PracticalExperience from "./components/PracticalExperience";
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isEditing: true,
-        };
-        this.displayInformation = this.displayInformation.bind(this);
-        this.editInformation = this.editInformation.bind(this);
-    }
-    displayInformation() {
-        this.setState({
-            isEditing: false,
-        });
-    }
-    editInformation() {
-        this.setState({
-            isEditing: true,
-        });
-    }
-    render() {
-        return (
-            <div>
-                <h1>CV Application</h1>
-                <form className="application-form" action="">
-                    <GeneralInformation isEditing={this.state.isEditing} />
-                    <EducationalExperience isEditing={this.state.isEditing} />
-                    <PracticalExperience isEditing={this.state.isEditing} />
-                </form>
-                <div id="cta-buttons">
-                    <button id="submit-btn" onClick={this.displayInformation}>
-                        Submit
-                    </button>
-                    <button id="edit-btn" onClick={this.editInformation}>
-                        Edit
-                    </button>
-                </div>
+const App = (props) => {
+    const [editing, setEditing] = useState(true);
+    const displayInformation = () => setEditing(false);
+    const editInformation = () => setEditing(true);
+    return (
+        <div>
+            <h1>CV Application</h1>
+            <form className="application-form" action="">
+                <GeneralInformation editing={editing} />
+                <EducationalExperience editing={editing} />
+                <PracticalExperience editing={editing} />
+            </form>
+            <div id="cta-buttons">
+                <button id="submit-btn" onClick={displayInformation}>
+                    Submit
+                </button>
+                <button id="edit-btn" onClick={editInformation}>
+                    Edit
+                </button>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default App;
