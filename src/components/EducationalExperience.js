38a5dc31a79ebc7f2a8caf804814fec0c1a9f6e4
Slys <slysdeveloper@gmail.com>
2023-06-09 13:59:16 -0400

    Converted Educational Experience section --> functional component

--- src/components/EducationalExperience.js
+++ src/components/EducationalExperience.js
@@ -1,90 +1,75 @@
-import { Component } from "react";
+import { useState } from "react";
 import "../styles/ApplicationSections.css";
 
-export default class EducationalExperience extends Component {
-    constructor(props) {
-        super(props);
+const EducationalExperience = ({ editing }) => {
+    const [schoolName, setSchoolName] = useState("");
+    const [major, setMajor] = useState("");
+    const [graduationYear, setGraduationYear] = useState(undefined);
+    if (editing) {
+        return (
+            <div className="form-section-container">
+                <h2>Educational Experience</h2>
+                <div className="section-information">
+                    <div className="form-item">
+                        <p>Name of School:</p>
+                        <input
+                            value={schoolName}
+                            onChange={(e) => {
+                                setSchoolName(e.target.value);
+                            }}
+                            type="text"
+                            name="schoolName"
+                            id=""
+                        />
+                    </div>
+                    <div className="form-item">
+                        <p>Major:</p>
+                        <input
+                            value={major}
+                            onChange={(e) => {
+                                setMajor(e.target.value);
+                            }}
+                            type="text"
+                            name="major"
+                            id=""
+                        />
+                    </div>
+                    <div className="form-item">
+                        <p>Graduation Year:</p>
 
-        this.state = {
-            schoolName: "",
-            major: "",
-            graduationYear: undefined,
-        };
-    }
-
-    render() {
-        const { isEditing } = this.props;
-        if (isEditing) {
-            return (
-                <div className="form-section-container">
-                    <h2>Educational Experience</h2>
-                    <div className="section-information">
-                        <div className="form-item">
-                            <p>Name of School:</p>
-                            <input
-                                value={this.state.schoolName}
-                                onChange={(e) => {
-                                    this.setState({
-                                        schoolName: e.target.value,
-                                    });
-                                }}
-                                type="text"
-                                name="schoolName"
-                                id=""
-                            />
-                        </div>
-                        <div className="form-item">
-                            <p>Major:</p>
-                            <input
-                                value={this.state.major}
-                                onChange={(e) => {
-                                    this.setState({
-                                        major: e.target.value,
-                                    });
-                                }}
-                                type="text"
-                                name="major"
-                                id=""
-                            />
-                        </div>
-                        <div className="form-item">
-                            <p>Graduation Year:</p>
-
-                            <input
-                                value={this.state.graduationYear}
-                                onChange={(e) => {
-                                    this.setState({
-                                        graduationYear: e.target.value,
-                                    });
-                                }}
-                                type="number"
-                                name="graduationYear"
-                                id=""
-                            />
-                        </div>
+                        <input
+                            value={graduationYear}
+                            onChange={(e) => {
+                                setGraduationYear(e.target.value);
+                            }}
+                            type="number"
+                            name="graduationYear"
+                            id=""
+                        />
                     </div>
                 </div>
-            );
-        } else {
-            return (
-                <div className="form-section-container">
-                    <h2>Educational Experience</h2>
-                    <div className="section-information">
-                        <div className="form-item">
-                            <p>Name of School:</p>
-                            <p>{this.state.schoolName}</p>
-                        </div>
-                        <div className="form-item">
-                            <p>Major:</p>
-                            <p>{this.state.major}</p>
-                        </div>
-                        <div className="form-item">
-                            <p>Graduation Year:</p>
-                            <p>{this.state.graduationYear}</p>
-                        </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="form-section-container">
+                <h2>Educational Experience</h2>
+                <div className="section-information">
+                    <div className="form-item">
+                        <p>Name of School:</p>
+                        <p>{schoolName}</p>
+                    </div>
+                    <div className="form-item">
+                        <p>Major:</p>
+                        <p>{major}</p>
+                    </div>
+                    <div className="form-item">
+                        <p>Graduation Year:</p>
+                        <p>{graduationYear}</p>
                     </div>
                 </div>
-            );
-        }
+            </div>
+        );
     }
-}
+};
+export default EducationalExperience;
